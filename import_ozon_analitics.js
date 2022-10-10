d8ecaabc466e336c71f7d77e4d9ef4c3fb381d56
istoyakin <59052518+istoyakin@users.noreply.github.com>
2022-10-10 13:58:49 +0300

    Update import_ozon_analitics.js

--- import_ozon_analitics.js
+++ import_ozon_analitics.js
@@ -1,7 +1,8 @@
 function OZON_import() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  /// Объявляем перменные книги, листа
               var ss = SpreadsheetApp.getActiveSpreadsheet();
               var sheet = ss.getSheetByName("OZON_API_import");
+  /// Выбираем диапазон дат для выгрузки
               var today = new Date();
               var dd = String(today.getDate()).padStart(2, '0');
               var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -16,17 +17,20 @@ function OZON_import() {
                 mm = String(mm).padStart(2,'0');                
               }
               var date_from = yyyy + '-' + mm + '-' + dd;
-              var j = Number(sheet.getRange(1,15).getValue());
+  /// Подготовливаем лист
+              var j = Number(sheet.getRange(1,15).getValue()); /// Переменная для запоминания точки итерации, если скрипт работал дольше 6 минут
               var r1 = sheet.getLastRow();
               var c1 = sheet.getMaxColumns();
-              if (r1<2){r1 = 2}
+              if (r1<2){r1 = 2} /// Не трогаем 1ю строку
               if (j==0) 
               {sheet.getRange(2,1,r1,c1).clearContent();}
               else
-              {sheet.getRange((j-1)*1000+2,1,r1,c1).clearContent();}
-          for (var iter = 1000; iter == 1000; j = j+1){
-          var offset = j*1000;
-          sheet.getRange(1,15).setValue(j);
+              {sheet.getRange((j-1)*1000+2,1,r1,c1).clearContent();} /// Если скрпит прервался, удаляем данные до последней успешной итерации
+  /// Начинаем итерацию
+          for (var iter = 1000; iter == 1000; j = j+1){ /// Выполняем пока количество строк в ответе не станет отличным от 1000 (максимальное количество строк в итерации)
+          var offset = j*1000; /// Отступ в ответе от API
+          sheet.getRange(1,15).setValue(j); /// записываем номер итерации
+  /// Параметры API запроса
                 var data = {
                   "date_from": date_from,
                   "date_to": today,
@@ -58,14 +62,17 @@ function OZON_import() {
                   'Api-Key': ''
                   }
                 }
+   ///Запрос API
                 var response = UrlFetchApp.fetch('https://api-seller.ozon.ru/v1/analytics/data', options);
                 var response = JSON.parse(response.getContentText());
+   ///Создаем массив для записи в него данных
                 var response_iter = new Array(response.result.data.length);
                 for (var k=0; k<=response.result.data.length-1 ;k=k+1)
                 {
                   response_iter[k] = new Array(6);
                 }
                 var lastRow = sheet.getLastRow();
+   /// Записываем нужные данные
               for (i=0; i<=response.result.data.length-1; i=i+1) {
                 response_iter[i][0] = response.result.data[i].dimensions[0].id;
                 response_iter[i][1] = response.result.data[i].dimensions[1].id;
@@ -74,12 +81,17 @@ function OZON_import() {
                 response_iter[i][4] = response.result.data[i].metrics[2];
                 response_iter[i][5] = response.result.data[i].metrics[3];
               }
+  /// Записываем данные в таблицу
               sheet.getRange(lastRow+1,1,response_iter.length,6).setValues(response_iter);
+  /// Записываем колиство строк для последующей проверки в цикле
               iter = response.result.data.length;
+  /// Чистим массив
               response_iter.splice(0,response_iter.length);
               
 }
+  /// Если скрипт завершен успешно - переносим данные в другую таблицу (чтобы таблица не была в постоянном обновлении)
 copy_clear(lastRow+iter,6,"OZON_API_Import","import2");
+  /// Обновляем показатель итерации
 sheet.getRange(1,15).setValue("0");
 
 }
